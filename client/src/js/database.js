7e34ebbcd9a202fe1c007af048067fabb1970b6b
levigun <t.levina31@gmail.com>
2022-04-20 20:23:39 +1000

    refactor some code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,12 +27,12 @@ export const putDb = async (content) => {
 export const getDb = async () => {
   console.log('GET all from the database');
   const jateDB = await openDB ('jate', 1);
-  const tx = jateDB.transaction('jate', 'readonly');
+  const tx = jateDB.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result)
-  return result;
+  // return result;
 };
 
 
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,13 +1,13 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// NOTE TO SELF: WATCH THE MANIFEST LESSON!!
+console.log('butInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     // store the triggered event
     window.deferredPrompt = event;
-
+    console.log("before!");
     // remove the hidden class from the button
     butInstall.classList.toggle('hidden', false);
 });
@@ -15,16 +15,18 @@ window.addEventListener('beforeinstallprompt', (event) => {
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
-
+    console.log("click 1 !");
     if (!promptEvent) {
         return;
     }
+    console.log("click 2 !");
 
     // show prompt
     promptEvent.prompt();
 
     // resets the deferred prompt variable, it can only be used once
     window.deferredPrompt = null;
+   
 
     butInstall.classList.toggle('hidden', true);
 });
